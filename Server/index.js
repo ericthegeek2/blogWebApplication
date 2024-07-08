61e95a3ba4c49066ad4ad259ab2d262d5b3e0199
Eric Mwangi <em3514790@gmail.com>
2024-07-08 14:19:29 -0700

    blog web application

--- Server/index.js
+++ Server/index.js
@@ -3,4 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api', (req,res) => res.json({status: 'ok'}))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
